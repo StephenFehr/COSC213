bc1883c855a7fcb74bf80d02f57034a0c290d84c
StephenFehr <94208568+StephenFehr@users.noreply.github.com>
2021-11-30 21:22:53 -0800

    Add files via upload

---
+++ findPlanes.php
@@ -0,0 +1,23 @@
+<?php
+    $mysqli = mysqli_connect("localhost", "cs213user", "letmein", "airfield");
+    $sql = "SELECT * from planes;";
+    $result = mysqli_query($mysqli, $sql) or die(mysqli_error($mysqli));
+               
+    if (mysqli_num_rows($result)< 1){
+        header ("Location: bookings.php");
+    }
+    else {
+        echo "<h2> Available Planes: </h2>";
+        echo "<pre>";
+        printf("Tail Number\tModel\t\t\tSeats\tGVW\t\tCatergory\n");
+        printf("%'-85s\n","");
+        while ($row = mysqli_fetch_array($result)){
+            printf("%s\t%-24s%s\t%s\t%s\n", $row['plane_id'],$row['model'],
+                   $row['seats'],$row['gvw'],$row['category']);
+        
+                     } //while     
+        echo "</pre>";
+     
+    } //else   
+?>
+
