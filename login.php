e4eb97968b3b90d9da9ed290978253eefc5d0cbe
DiamondDaveT <95320732+DiamondDaveT@users.noreply.github.com>
2021-11-30 20:04:30 -0800

    Update login.php

--- login.php
+++ login.php
@@ -7,7 +7,6 @@ session_start();
 if (isset($_POST["submit"])) {
     $message = "Flight is available for booking. Please log in or create account to confirm booking.";  
 } else {
-    echo 'Unauthorized user, please try again.';
     header("Location: login.php");
     exit;
 }
