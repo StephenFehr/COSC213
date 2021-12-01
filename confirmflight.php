af76daf4e935fd5a150849162ddc740c2d58744f
DiamondDaveT <95320732+DiamondDaveT@users.noreply.github.com>
2021-11-30 19:51:05 -0800

    Update confirmflight.php

--- confirmflight.php
+++ confirmflight.php
@@ -1,3 +1,28 @@
 <?php
-echo "this is the confirmation page";
+session_start();
+if((!filter_input(INPUT_POST, 'validEmail')) || (!filter_input(INPUT_POST, 'validPassword')))
+{
+  header("Location: login.php");
+  exit;
+}
+//check database for valid email and password fields
+$mysqli = mysqli_connect("localhost", "cs213user", "letmein", "airfieldDB");
+$validEmail = filter_input(INPUT_POST, 'validEmail');
+$validPassword = filter_input(INPUT_POST, 'validPassword');
+
+//query database
+$sql = "SELECT email, password FROM users WHERE email = '".$validEmail."' AND password = SHA1('".$validPassword."')";
+
+//get results from valid input query
+$result = mysqli_query($mysqli, $sql) or die(mysqli_error($mysqli));
+if(mysqli_num_rows($result) == 1){
+  $authenticated = "User Authenticated";
+}
+else
+{
+  header("Location: login.php");
+  exit;
+}
 ?>
+
+
