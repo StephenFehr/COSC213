90902fef2d6efb2677c391465dcd783c30990a39
DiamondDaveT <95320732+DiamondDaveT@users.noreply.github.com>
2021-12-01 10:17:23 -0800

    Update createAccount.php

--- createAccount.php
+++ createAccount.php
@@ -56,7 +56,7 @@ if (mysqli_num_rows($result) == 1 && isset($_POST["create_account"])) {
     $sql = "INSERT INTO users VALUES ('$firstname', '$lastname', '$email', SHA1('$password'))";
     if ($connect->query($sql) == true && isset($_POST['create_account'])) {
         echo '<h3>Thank you '.$firstname.' '.$lastname.', your account ' . $email . ' has been created. Please login to book a flight.</h3>';
-        echo '<a href="login.php">Go To Login</a>';
+        echo '<a href="login.html">Go To Login</a>';
     }
 }
 ?>
