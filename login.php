29c76dad82a5909bbc0f5f002c0edb390890918e
DiamondDaveT <95320732+DiamondDaveT@users.noreply.github.com>
2021-11-30 22:44:11 -0800

    Update login.php

--- login.php
+++ login.php
@@ -41,7 +41,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Other/html.html to edit this temp
         <!-- content -->
         <br>
         <div>
-            <form method="post" action="confirmFlight.php">
+            <form method="post" action="payment.php">
                 <h3><?php echo $message; ?><h3>
                 <h4 style="color: red;"><?php echo $error; ?></h4>
                 <label for="validEmail"><strong>Email:</strong></label>
