1184e9afb8fd01affa8316df9ec49b29b51cf137
DiamondDaveT <95320732+DiamondDaveT@users.noreply.github.com>
2021-12-01 15:36:22 -0800

    Update aircraft.php

--- aircraft.php
+++ aircraft.php
@@ -2,3 +2,27 @@
 
 
 ?>
+<html>
+    <head>
+        <title>Book a Flight</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">        
+    </head>
+    <body>
+        <!-- header -->
+        <div class="logo">
+            <img src="images/logo.png" alt="Logo" style="width: 400px; height: 100px;">
+        </div>
+        <h1><i><Strong>Ubuntu Regional Airport</strong></i></h1>
+        <div class="header">
+            <a href="homepage.php">Home</a>
+            <a href="aircraft.php" style="pointer-events: none;">Aircraft</a>
+            <a href="scheduling.php">Scheduling</a>
+            <a href="login.php">Logout</a>
+        </div>
+        <img id="titleImage" src="images/airplane.png" alt="Runway Image">
+        <!-- content -->
+        <h2>Featured Aircraft.</h2>
+    </body>
+</html>
