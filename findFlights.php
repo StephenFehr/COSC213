1e017e3474a2e08ea81c0ddb0e3ac309fbff629f
StephenFehr <94208568+StephenFehr@users.noreply.github.com>
2021-12-01 14:05:07 -0800

    Update findFlights.php

--- findFlights.php
+++ findFlights.php
@@ -1,5 +1,7 @@
 <?php
+     
     $id = (int) $_GET["PlaneID"];
+echo "<p>".$id."</p>";
     $mysqli = mysqli_connect("localhost", "cs213user", "letmein", "airfield");
     $sql = "SELECT * FROM flights f, planes p, pilots pi 
 			WHERE p.plane_id = f.plane_id AND pi.pilot_id = f.pilot_id AND f.plane_id = $id";
