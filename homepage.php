151f0ec077879c58a0a7afbfb370a0901d77ef8d
DiamondDaveT <95320732+DiamondDaveT@users.noreply.github.com>
2021-12-01 09:08:17 -0800

    Update and rename homepage.html to homepage.php

--- homepage.php
+++ homepage.php
@@ -1,3 +1,43 @@
+<?php
+session_start();
+if((!filter_input(INPUT_POST, 'validEmail')) || (!filter_input(INPUT_POST, 'validPassword')))
+{
+  header("Location: login.php");
+  exit;
+}
+//check database for valid email and password fields
+$mysqli = mysqli_connect("localhost", "cs213user", "letmein", "airfieldDB");
+$validEmail = filter_input(INPUT_POST, 'validEmail');
+$validPassword = filter_input(INPUT_POST, 'validPassword');
+
+//query database
+$sql = "SELECT firstname, lastname, email, password FROM users WHERE email = '".$validEmail."' AND password = SHA1('".$validPassword."')";
+
+//get results from valid input query
+$result = mysqli_query($mysqli, $sql) or die(mysqli_error($mysqli));
+if(mysqli_num_rows($result) == 1){
+  
+  //get user information
+  while($info = mysqli_fetch_array($result))
+  {
+    $email = stripslashes($info["email"]);
+    $firstname = stripslashes($info["firstname"]);
+    $lastname = stripslashes($info["lastname"]);
+  }
+  $_SESSION["email"] = $email;
+  $_SESSION["auth_user"] = $firstname." ".$lastname;
+  $authenticated = "Welcome ".$_SESSION["auth_user"].", please confirm your purchase details.";
+  
+  //set authorization cookie
+  setcookie("auth", session_id(), time() + 60 * 30, "/", "", 0);
+}
+else
+{
+  header("Location: login.php");
+  exit;
+}
+$total;
+?>
 <!DOCTYPE html>
 <!--
 Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
