1a8869ecea39c6afdc80d993e5bb1bfcdf927216
StephenFehr <94208568+StephenFehr@users.noreply.github.com>
2021-12-01 11:58:17 -0800

    Update findFlights.php

--- findFlights.php
+++ findFlights.php
@@ -1,7 +1,8 @@
 <?php
-    $id = (int) $_GET["flighID"];  // or $id = (int) $_POST["facultyid"];
+    $id = (int) $_GET["flightID"];
     $mysqli = mysqli_connect("localhost", "cs213user", "letmein", "airfield");
-    $sql = "SELECT * FROM flights WHERE flights.flight_id = $id";
+    $sql = "SELECT * FROM flights f, planes p, pilots pi 
+			WHERE p.plane_id = f.plane_id AND pi.pilot_id = f.pilot_id AND f.flight_id = $id";
     $result = mysqli_query($mysqli, $sql) or die(mysqli_error($mysqli));
                
     if (mysqli_num_rows($result)< 1){
@@ -9,11 +10,11 @@
                  }
     else {
         $row = mysqli_fetch_array($result);
-        $resultstr = "<h2> Flights from ".$row['pilot_id'].", Tail number: ".$row['plane_id']."</h2>";
+        $resultstr = "<h2> Flights from ".$row['p.model'].", Tail number: ".$row['plane_id']."</h2>";
         $resultstr .= "<pre>";
-        $resultstr .= sprintf("Flight Number\t\tDate\n");
+        $resultstr .= sprintf("Flight Number\t\tDate\t\Pilot\t\t\f.plane_id\n");
         $resultstr .= sprintf("%'-55s\n","");
-        $resultstr .= sprintf("%s\t%s\t%s\n",$row['flight_id'], $row['schedule']);
+        $resultstr .= sprintf("%s\t%s\t%s\n",$row['flight_id'], $row['schedule'], $row['pi.fname'], $row['plane_id']);
         while ($row = mysqli_fetch_array($result)){
             $resultstr .= sprintf("%s\t%s\t%s\n",$row['flight_id'], $row['schedule']);
         } //while   
