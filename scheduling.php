63fb5f16edbc7645bbb5b3e941cfc0441458c347
DiamondDaveT <95320732+DiamondDaveT@users.noreply.github.com>
2021-12-01 22:30:31 -0800

    Update scheduling.php

--- scheduling.php
+++ scheduling.php
@@ -1,9 +1,10 @@
-<?php session_start();
-      if ( !isset($_SESSION["loggedin"])){
-          header("Location: login.php");
-          exit;
-      }
-      $_SESSION["loggedin"] = true;
+<?php 
+session_start();
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true)
+{
+    header("Location: login.php");
+    exit;
+}
 ?>
 
 <html>
