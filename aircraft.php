210936e77c3a18f437acc2b08e49616b486fb128
DiamondDaveT <95320732+DiamondDaveT@users.noreply.github.com>
2021-12-01 22:26:37 -0800

    Update aircraft.php

--- aircraft.php
+++ aircraft.php
@@ -1,4 +1,11 @@
-<?php $_SESSION["loggedin"] = true; ?>
+<?php 
+session_start();
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true)
+{
+    header("Location: login.php");
+    exit;
+}
+?>
 
 <html>
     <head>
