cae8b5b6b6b09974bde9760a74fd4e2ac5e0e77d
DiamondDaveT <95320732+DiamondDaveT@users.noreply.github.com>
2021-12-01 16:40:45 -0800

    Update aircraft.php

--- aircraft.php
+++ aircraft.php
@@ -7,7 +7,7 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script> 
         $(document).ready(function(){
-          $(".aircraft").hover(function(){
+          $("#cessna").hover(function(){
             $(this).animate({opacity: 0.5}, 500);
             }, function () {
               $(this).animate({opacity: 1.0}, 500);
