694d65c1e556a537898d7eb366b92be656b71de4
DiamondDaveT <95320732+DiamondDaveT@users.noreply.github.com>
2021-12-01 21:45:29 -0800

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -7,8 +7,6 @@ if((!filter_input(INPUT_POST, 'validEmail')) || (!filter_input(INPUT_POST, 'vali
     header("Location: login.php");
     exit;
   }
-  else
-    $_SESSION["loggedin"] = true;
 }
 
 //check database for valid email and password fields
