7a56024c066abbe1d88df285f3576052ca0dfb4c
DiamondDaveT <95320732+DiamondDaveT@users.noreply.github.com>
2021-12-02 14:17:38 -0800

    Update scheduling.php

--- scheduling.php
+++ scheduling.php
@@ -55,7 +55,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true)
             <a href="homepage.php">Home</a>
             <a href="aircraft.php">Aircraft</a>
             <a href="scheduling.php" style="pointer-events: none;">Scheduling</a>
-            <a href="login.php">Logout</a>
+            <a href="logout.php">Logout</a>
         </div>
         <img id="titleImage" src="images/airplane.png" alt="Runway Image">
         <!-- content -->
