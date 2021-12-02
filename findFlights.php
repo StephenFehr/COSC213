77f05ca753afb1f2cf31f871318af4a157b013f3
DiamondDaveT <95320732+DiamondDaveT@users.noreply.github.com>
2021-12-01 22:35:25 -0800

    Update findFlights.php

--- findFlights.php
+++ findFlights.php
@@ -1,4 +1,10 @@
 <?php
+    session_start();
+    if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true)
+    {
+        header("Location: login.php");
+        exit;
+    }
     $id = $_GET["PlaneID"];
     $mysqli = mysqli_connect("localhost", "cs213user", "letmein", "airfield");
     $sql = "SELECT * FROM flights f, planes p, pilots pi WHERE p.plane_id = f.plane_id AND pi.pilot_id = f.pilot_id AND f.plane_id = '".$id."'";
