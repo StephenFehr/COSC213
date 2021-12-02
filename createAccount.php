dd436b7be2d9486a3cc7fde4b3cdb8dbc20c9feb
DiamondDaveT <95320732+DiamondDaveT@users.noreply.github.com>
2021-12-01 18:05:31 -0800

    Update createAccount.php

--- createAccount.php
+++ createAccount.php
@@ -1,29 +1,3 @@
-<!DOCTYPE html>
-<!--
-Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-Click nbfs://nbhost/SystemFileSystem/Templates/Other/html.html to edit this template
--->
-<html>
-    <head>
-        <title>Create Account</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="style.css">
-    </head>
-    <body>
-        <!-- header -->
-        <div class="logo">
-            <img src="images/logo.png" alt="Logo" style="width: 400px; height: 100px;">
-        </div>
-        <h1 class="regional"><i><Strong>Ubuntu Regional Airport</strong></i></h1>
-        <div class="header">
-            <a href="login.php">Return to Login</a>
-        </div>
-        <img id="titleImage" src="images/airplane.png" alt="Runway Image">
-        <!-- content -->
-        <div>
-    </body>
-</html>
 <?php
 session_start();
 $firstname = $_POST['firstname'];
@@ -60,13 +34,30 @@ if (mysqli_num_rows($result) == 1 && isset($_POST["create_account"])) {
     }
 }
 ?>
-
 <!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Other/html.html to edit this template
+-->
 <html>
     <head>
         <title>Create Account</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
     </head>
-    <body style="background-color: bisque">
+    <body>
+        <!-- header -->
+        <div class="logo">
+            <img src="images/logo.png" alt="Logo" style="width: 400px; height: 100px;">
+        </div>
+        <h1 class="regional"><i><Strong>Ubuntu Regional Airport</strong></i></h1>
+        <div class="header">
+            <a href="login.php">Return to Login</a>
+        </div>
+        <img id="titleImage" src="images/airplane.png" alt="Runway Image">
+        <!-- content -->
+        <div>
         <h2>Please create account.</h2>
         <form name="form" method="post" action="<?php echo $PHP_SELF; ?>">
             <fieldset>
