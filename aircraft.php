ca3e7e4502c4bac8546cf94f6876283d80ef77c4
DiamondDaveT <95320732+DiamondDaveT@users.noreply.github.com>
2021-12-01 19:27:00 -0800

    Update aircraft.php

--- aircraft.php
+++ aircraft.php
@@ -78,7 +78,7 @@
         <img id="titleImage" src="images/airplane.png" alt="Runway Image">
         <!-- content -->
         <h2 id="aircraft">Featured Aircraft.</h2>
-        <h2 id="plane"></h2>
+        <h2 id="plane" style="display: inline-block;"></h2>
         <div class="aircraft">
             <img id="cessna" src="images/cessna150.jpeg" alt="Cessna 150 Airplane Image" style="width: 500px; height: 400px;">
             <img id="harbin" src="images/harbinY12.jpg" alt="Harbin Y12 Airplane Image" style="width: 500px; height: 400px;">
